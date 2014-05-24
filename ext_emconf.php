dc2f7b3afb8221742b9c726e0fe0f153021e665b
Daniel Corn <d@cundd.net>
2014-05-24 12:39:59 +0200

    Added handler file

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2',
+			'typo3' => '6.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
