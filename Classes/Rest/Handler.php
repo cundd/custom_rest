a037969f8cdfe4a8484790546ebe8dfaa275f7ed
bendash <bendash@users.noreply.github.com>
2016-09-05 17:20:31 +0200

    Update Handler.php

--- Classes/Rest/Handler.php
+++ Classes/Rest/Handler.php
@@ -116,4 +116,43 @@ class Handler implements HandlerInterface {
 			});
 		});
 	}
+	
+	/**
+	 * calls a extbase plugin
+	 * 
+	 * @param string $pluginName the name of the plugin like configured in ext_localconf.php
+	 * @param string $vendorName the name of the vendor (if no vendor use '')
+	 * @param string $extensionName the name of the extension
+	 * @param string $actionName the name of the action to call
+	 * @param array $arguments the arguments to pass to the action
+	 *
+	 * @return string
+	 */
+	protected function callExtbasePlugin($pluginName, $vendorName, $extensionName, $controllerName, $actionName, $arguments) {
+	
+		$pluginNamespace = strtolower('tx_'. $extensionName . '_' . $pluginName);
+		
+		$_POST[$pluginNamespace]['controller'] = $controllerName;
+		$_POST[$pluginNamespace]['action'] = $actionName;
+	       
+		$keys = array_keys($arguments);
+		foreach ($keys as $key) {
+			$_POST[$pluginNamespace][$key] = $arguments[$key];
+		}
+	    
+		$configuration = [
+			'extensionName' => $extensionName,
+			'pluginName' => $pluginName
+		];
+	       
+		if (!empty($vendorName)) {
+			$configuration['vendorName'] = $vendorName;
+		}
+	       
+		$bootstrap = $this->objectManager->get(\TYPO3\CMS\Extbase\Core\Bootstrap::class);
+	    
+		$response = $bootstrap->run('', $configuration);
+	       
+		return $response;
+	}
 }
