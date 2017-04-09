3697101488075bee158c01bdd09504533e60346c
Daniel Corn <info@cundd.net>
2017-04-09 15:36:07 +0200

    Removed unused code

--- ext_tables.php
+++ ext_tables.php
@@ -9,33 +9,5 @@ if (!defined('TYPO3_MODE')) {
     'myPlugin - List '
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Custom REST extensions');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_customrest_domain_model_person', 'EXT:custom_rest/Resources/Private/Language/locallang_csh_tx_customrest_domain_model_person.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_customrest_domain_model_person');
-//$GLOBALS['TCA']['tx_customrest_domain_model_person'] = array(
-//	'ctrl' => array(
-//		'title'	=> 'LLL:EXT:custom_rest/Resources/Private/Language/locallang_db.xlf:tx_customrest_domain_model_person',
-//		'label' => 'first_name',
-//		'tstamp' => 'tstamp',
-//		'crdate' => 'crdate',
-//		'cruser_id' => 'cruser_id',
-//		'dividers2tabs' => TRUE,
-//
-//		'versioningWS' => 2,
-//		'versioning_followPages' => TRUE,
-//
-//		'languageField' => 'sys_language_uid',
-//		'transOrigPointerField' => 'l10n_parent',
-//		'transOrigDiffSourceField' => 'l10n_diffsource',
-//		'delete' => 'deleted',
-//		'enablecolumns' => array(
-//			'disabled' => 'hidden',
-//			'starttime' => 'starttime',
-//			'endtime' => 'endtime',
-//		),
-//		'searchFields' => 'first_name,last_name,birthday,',
-//		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Person.php',
-//		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_customrest_domain_model_person.gif'
-//	),
-//);
