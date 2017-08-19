33a1b2131dc1610b02e2b5976184178c11018a4a
Daniel Corn <info@cundd.net>
2017-08-19 20:23:40 +0200

    Code formatting

--- Configuration/TCA/tx_customrest_domain_model_person.php
+++ Configuration/TCA/tx_customrest_domain_model_person.php
@@ -4,7 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 return [
-    'ctrl'      =>
+    'ctrl' =>
         [
             'title'         => 'LLL:EXT:custom_rest/Resources/Private/Language/locallang_db.xlf:tx_customrest_domain_model_person',
             'label'         => 'first_name',
@@ -20,138 +20,134 @@ return [
             'transOrigPointerField'    => 'l10n_parent',
             'transOrigDiffSourceField' => 'l10n_diffsource',
             'delete'                   => 'deleted',
-            'enablecolumns'            => array(
+            'enablecolumns'            => [
                 'disabled'  => 'hidden',
                 'starttime' => 'starttime',
                 'endtime'   => 'endtime',
-            ),
+            ],
             'searchFields'             => 'first_name,last_name,birthday,',
             'iconfile'                 => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('custom_rest')
                 . 'Resources/Public/Icons/tx_customrest_domain_model_person.gif',
-        ]
-    ,
-    'interface' => array(
-        'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, first_name, last_name, birthday',
-    ),
-    'types'     => array(
-        '1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, first_name, last_name, birthday, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'),
-    ),
-    'palettes'  => array(
-        '1' => array('showitem' => ''),
-    ),
-    'columns'   => array(
+        ],
 
-        'sys_language_uid' => array(
+    'interface' => [
+        'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, first_name, last_name, birthday',
+    ],
+    'types'     => [
+        '1' => ['showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, first_name, last_name, birthday, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'],
+    ],
+    'palettes'  => [
+        '1' => ['showitem' => ''],
+    ],
+
+    'columns' => [
+        'sys_language_uid' => [
             'exclude' => 1,
             'label'   => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
-            'config'  => array(
+            'config'  => [
                 'type'                => 'select',
                 'foreign_table'       => 'sys_language',
                 'foreign_table_where' => 'ORDER BY sys_language.title',
-                'items'               => array(
-                    array('LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages', -1),
-                    array('LLL:EXT:lang/locallang_general.xlf:LGL.default_value', 0),
-                ),
-            ),
-        ),
-        'l10n_parent'      => array(
+                'items'               => [
+                    ['LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages', -1],
+                    ['LLL:EXT:lang/locallang_general.xlf:LGL.default_value', 0],
+                ],
+            ],
+        ],
+        'l10n_parent'      => [
             'displayCond' => 'FIELD:sys_language_uid:>:0',
             'exclude'     => 1,
             'label'       => 'LLL:EXT:lang/locallang_general.xlf:LGL.l18n_parent',
-            'config'      => array(
+            'config'      => [
                 'type'                => 'select',
-                'items'               => array(
-                    array('', 0),
-                ),
+                'items'               => [
+                    ['', 0],
+                ],
                 'foreign_table'       => 'tx_customrest_domain_model_person',
                 'foreign_table_where' => 'AND tx_customrest_domain_model_person.pid=###CURRENT_PID### AND tx_customrest_domain_model_person.sys_language_uid IN (-1,0)',
-            ),
-        ),
-        'l10n_diffsource'  => array(
-            'config' => array(
+            ],
+        ],
+        'l10n_diffsource'  => [
+            'config' => [
                 'type' => 'passthrough',
-            ),
-        ),
-
-        't3ver_label' => array(
+            ],
+        ],
+        't3ver_label'      => [
             'label'  => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
-            'config' => array(
+            'config' => [
                 'type' => 'input',
                 'size' => 30,
                 'max'  => 255,
-            ),
-        ),
-
-        'hidden'    => array(
+            ],
+        ],
+        'hidden'           => [
             'exclude' => 1,
             'label'   => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
-            'config'  => array(
+            'config'  => [
                 'type' => 'check',
-            ),
-        ),
-        'starttime' => array(
+            ],
+        ],
+        'starttime'        => [
             'exclude'   => 1,
             'l10n_mode' => 'mergeIfNotBlank',
             'label'     => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
-            'config'    => array(
+            'config'    => [
                 'type'     => 'input',
                 'size'     => 13,
                 'max'      => 20,
                 'eval'     => 'datetime',
                 'checkbox' => 0,
                 'default'  => 0,
-                'range'    => array(
+                'range'    => [
                     'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y')),
-                ),
-            ),
-        ),
-        'endtime'   => array(
+                ],
+            ],
+        ],
+        'endtime'          => [
             'exclude'   => 1,
             'l10n_mode' => 'mergeIfNotBlank',
             'label'     => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
-            'config'    => array(
+            'config'    => [
                 'type'     => 'input',
                 'size'     => 13,
                 'max'      => 20,
                 'eval'     => 'datetime',
                 'checkbox' => 0,
                 'default'  => 0,
-                'range'    => array(
+                'range'    => [
                     'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y')),
-                ),
-            ),
-        ),
-
-        'first_name' => array(
+                ],
+            ],
+        ],
+        'first_name'       => [
             'exclude' => 1,
             'label'   => 'LLL:EXT:custom_rest/Resources/Private/Language/locallang_db.xlf:tx_customrest_domain_model_person.first_name',
-            'config'  => array(
+            'config'  => [
                 'type' => 'input',
                 'size' => 30,
                 'eval' => 'trim',
-            ),
-        ),
-        'last_name'  => array(
+            ],
+        ],
+        'last_name'        => [
             'exclude' => 1,
             'label'   => 'LLL:EXT:custom_rest/Resources/Private/Language/locallang_db.xlf:tx_customrest_domain_model_person.last_name',
-            'config'  => array(
+            'config'  => [
                 'type' => 'input',
                 'size' => 30,
                 'eval' => 'trim',
-            ),
-        ),
-        'birthday'   => array(
+            ],
+        ],
+        'birthday'         => [
             'exclude' => 1,
             'label'   => 'LLL:EXT:custom_rest/Resources/Private/Language/locallang_db.xlf:tx_customrest_domain_model_person.birthday',
-            'config'  => array(
+            'config'  => [
                 'dbType'   => 'date',
                 'type'     => 'input',
                 'size'     => 7,
                 'eval'     => 'date',
                 'checkbox' => 0,
                 'default'  => '0000-00-00',
-            ),
-        ),
-
-    ),
+            ],
+        ],
+    ],
 ];
